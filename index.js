78662b61fba902f9176d27eaf194244677fd649f
Daniel Castro <castrodd@gmail.com>
2017-09-15 21:29:03 -0400

    Basic framework

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var http = require('http');
 var fs = require('fs');
-var path = require('path');
+var ErrorPage = require('error-page')
+var extract = require('./extract');
 
 var server = http.createServer(function (req, res) {
+  res.error = ErrorPage(req, res, {
+    "*": 'There was a problem with your request. Please try again.',
+    debug: false // show full stack traces, or just messages
+  })
   console.log('Responding to a request.');
-  var url = req.url;
-  var fileName = 'index.html';
 
-  if (url.length > 1) {
-    fileName = url.substring(1);
-  }
-  console.log(fileName);
-  var filePath = path.resolve(__dirname, 'app', fileName);
+  var filePath = extract(req.url);
   fs.readFile(filePath, function(err, data) {
     if (err) {
-      console.log('Error');
-    }
+      return res.error(404);
+    } else {
       res.end(data);
+    }
   })
 });
 
