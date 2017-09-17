b4eb394dd11ef268a2d1aacd26d2d4e8c05d1569
Daniel Castro <castrodd@gmail.com>
2017-09-17 11:34:15 -0400

    ws-client module

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -1,11 +1,2 @@
-'use strict';
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var ChatApp = function ChatApp() {
-  _classCallCheck(this, ChatApp);
-
-  console.log('Hello ES6');
-};
-
+import ChatApp from './app';
 new ChatApp();
